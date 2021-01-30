0652efa5ffc4a743ea8487897e4cb647c6a80a03
Derek <california.cajou85@gmail.com>
2021-01-29 18:30:30 -0800

    itemcard_add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function SideNav() {
   return (
     <div class="sidenav">
-      <img src="/index.html" alt="logo"/>
-      <h1><a href={null}>Home</a></h1>
-      <h1><a href={null}>Blog</a></h1>
+      <img src="/index.html" alt="logo" />
+      <h1>
+        <a href={null}>Home</a>
+      </h1>
+      <h1>
+        <a href={null}>Blog</a>
+      </h1>
       <button type="button">Logout</button>
     </div>
   );
@@ -18,17 +22,83 @@ function SearchBar() {
     </div>
   );
 }
+function OpenItem(){
+    alert("good");
+  return;
+}
+
+function Ibutton() {
+return(
+<div class="ibutton">
+   <button type="button" onClick={OpenItem}> I </button>
+  
+  </div>
+);
+}
+
+
+function ComBox(){
+  return(
+  <textarea class="combox" name="" id="" cols="25" rows="2" placeholder="type your comment">
+
+  </textarea>
+  );
+}
+
+function Deletebutton(){
+return(
+  <div class="deletebutton">
+   <button type="button"> delete </button>
+  </div>
+  );
+}
+
+function ItemCard(){
+return(
+  <div class="itemCard">
+      
+  </div>
+);
+
+}
+
+function ItemBox() {
+  return (
+    <div class="itemBox">
+      <img id ="img1" src=""/> 
+    </div>
+
+  );
+
+
+}
+
+
+
+function App() {
+  return (
+    <div class="App">
+      <SideNav />
+      <Main />
+      <LeftSide />
+    </div>
+  );
+}
 
 function Main() {
   return (
     <div class="Main">
       <SearchBar />
-      
+      <ItemCard/>
+      <ItemBox/>
+      <Ibutton/>
+      <Deletebutton/>
+      <ComBox/>
     </div>
   );
 }
 
-function LeftSide () {
+function LeftSide() {
   return (
     <div class="leftside">
       <div>
@@ -42,40 +112,32 @@ function LeftSide () {
         <FilterBox />
       </div>
     </div>
-  )
+  );
 }
 
 function FilterBox() {
   return (
     <div>
       <div class="filter-box">
-        <input type="checkbox" value="apparel"/>
+        <input type="checkbox" value="apparel" />
         <label>Apparel</label>
-        <hr/>
-        <input type="checkbox" value="electronic"/>
+        <hr />
+        <input type="checkbox" value="electronic" />
         <label>Electronics</label>
-        <hr/>
-        <input type="checkbox" value="books"/>
+        <hr />
+        <input type="checkbox" value="books" />
         <label>Books</label>
-        <hr/>
-        <input type="checkbox" value="lab-equipment"/>
+        <hr />
+        <input type="checkbox" value="lab-equipment" />
         <label>Lab Equipment</label>
-        <hr/>
-        <input type="checkbox" value="others"/>
+        <hr />
+        <input type="checkbox" value="others" />
         <label>Others</label>
       </div>
     </div>
   );
 }
 
-function App() {
-  return (
-    <div class="App">
-      <SideNav />
-      <Main />
-      <LeftSide />
-    </div>
-  );
-}
+
 
 export default App;
