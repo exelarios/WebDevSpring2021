b7ceee4b885fc153547448421792f41e6a4d34d0
daivuong <paulminhdai@gmail.com>
2021-02-10 21:19:40 -0800

    done cards in blog page

--- src/modules/Blog/Blog.js
+++ src/modules/Blog/Blog.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Blog.css';
+import ThreadCard from './ThreadCard';
 
 function Blog() {
     return (
-      <div class="container">
+      <div class="container" id="blogPage">
+        <ThreadCard title="Thread title 1" author="John Doe" topic="Electronics"/>
+        <ThreadCard title="Thread title 2" author="John Doe" topic="Others"/>
+        <ThreadCard title="Thread title 3" author="John Doe" topic="Housing"/>
+        <ThreadCard title="Thread title 4" author="John Doe" topic="Classes"/>
       </div>
     );
 }
