454e7691678bb29c9b7b25e04e4baac605cdf3c2
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2021-02-25 17:17:23 -0800

    get indivudal post

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -13,7 +13,7 @@ router.get("/search", protected, async (req, res) => {
     const page = Number(req.query.page) || 1;
 
     const keyword = req.query.keyword ? {
-        name: {
+        title: {
             $regex: req.query.keyword,
             $options: "i",
         }
@@ -81,4 +81,25 @@ router.post("/add", protected, async(req, res) => {
 
 });
 
+/**
+ * @route   GET api/post/{postId}
+ * @desc    Fetch an individual post
+ * @access  Private
+ */
+router.get("/:id", protected, async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+        res.json({
+            title: post.title,
+            topic: post.topic,
+            body: post.body,
+            postBy: req.user.id
+        });
+    } else {
+        res.status(404).json({
+            message: "Post doesn't exist in the database.",
+            success: false
+        });
+    }
+});
 module.exports = router;
