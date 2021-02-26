763c1c52b03ba4f160e5c045c56255b0537e8f14
daivuong <paulminhdai@gmail.com>
2021-02-25 17:28:27 -0800

    before merch

--- frontend/src/modules/Blog/ThreadCardModal.js
+++ frontend/src/modules/Blog/ThreadCardModal.js
@@ -8,9 +8,18 @@ class ThreadCardModal extends Component {
             <div className="modalScreen" style={{ display: (this.props.modalActive) ? 'flex' : 'none' }}>
                 <div className="modal blogModal">
                     <div class="threadPosting">
-                        <h2 className="threadTitle">Thread Title Here</h2>
-                        <p className="threadInfo" id="datetime">Posted: MM/DD/YYYY</p>
-                        <p className="threadInfo" id="threadTopic">Topic: Housing</p>
+                        <div class="threadDetail">
+                            <div>
+                                <h2 className="threadTitle">Thread Title Here</h2>
+                                <p className="threadInfo" id="datetime">Posted: MM/DD/YYYY</p>
+                                <p className="threadInfo" id="threadTopic">Topic: Housing</p>
+                            </div>
+                            <div className="threadWritter">
+                                <span>John Doe</span>
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXZlaUXIWozu3xqknYB3S9nknCPGFPAEVZLA&usqp=CAU" alt="logo"></img>
+                            </div>
+                        </div>
+                        
                         <p id="threadContain">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil tempora possimus harum recusandae, 
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil tempora possimus harum recusandae</p>
                     </div>
